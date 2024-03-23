d26fe8ad44d50778ce504a573432c214ac2fe0ad
viinz <42003446+dev-viinz@users.noreply.github.com>
2024-03-23 22:32:30 +0100

    day 2 part 1

---
+++ day 2/solution.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func getScanner(data []byte) *bufio.Scanner {
+	scanner := bufio.NewScanner(bytes.NewReader(data))
+	return scanner
+}
+
+func getMaxAmountOfColor(line string, color string) int {
+	words := strings.Split(line, " ")
+	//string array of name results
+	var results []int
+	for key, word := range words {
+		word = strings.TrimRight(word, ",")
+		word = strings.TrimRight(word, ";")
+		if word == color {
+			r, err := strconv.Atoi(words[key-1])
+			if err != nil {
+				log.Fatalf("Error converting string to int: %s", err)
+			}
+			results = append(results, r)
+		}
+	}
+	if len(results) == 0 {
+		return 0
+	}
+	return slices.Max(results)
+}
+
+func getGameNumber(line string) int {
+	words := strings.Split(line, " ")
+	for key, word := range words {
+		if word == "Game" {
+			r, err := strconv.Atoi(strings.TrimRight(words[key+1], ":"))
+			if err != nil {
+				log.Fatalf("Error converting string to int: %s", err)
+			}
+			return r
+		}
+	}
+	return 0
+}
+
+func main() {
+	fmt.Println("Reading input file...")
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatalf("Error reading input file: %s", err)
+	}
+	fmt.Println("Input file read successfully!")
+	count := 0
+	scanner := getScanner(input)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		amountOfRed := getMaxAmountOfColor(line, "red")
+		amountOfGreen := getMaxAmountOfColor(line, "green")
+		amountOfBlue := getMaxAmountOfColor(line, "blue")
+		if amountOfRed <= 12 && amountOfBlue <= 14 && amountOfGreen <= 13 {
+			count += getGameNumber(line)
+		}
+	}
+	fmt.Println("Sum of Games that are possible: ", count)
+}
